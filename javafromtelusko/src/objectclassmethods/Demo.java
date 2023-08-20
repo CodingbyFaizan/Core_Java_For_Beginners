48a3ca419c7b02bbb701acf881c79c322f79710d
CodingByFaizan <enrollwebsite@gmail.com>
2023-08-20 17:51:58 +0530

    20-08-2023

---
+++ javafromtelusko/src/objectclassmethods/Demo.java
@@ -0,0 +1,24 @@
+package objectclassmethods;
+
+class Laptop{
+	String model ;
+	int price;
+	
+	
+	public String toString() {
+		return "hey";
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Laptop obj1 = new Laptop();
+		
+		Laptop obj2 = new Laptop();
+		System.out.println(obj1);
+		
+		
+		
+
+	}
+}
